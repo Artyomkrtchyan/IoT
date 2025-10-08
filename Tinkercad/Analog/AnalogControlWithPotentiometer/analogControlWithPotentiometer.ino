13901baf5b92c2fd8309e8eff32427152c95e2cf
Artyom Mkrtchyan <i21.mkrtchyan.artyom@etud.ufar.am>
2025-10-08 18:07:13 +0400

    Pin changed

--- Tinkercad/Analog/AnalogControlWithPotentiometer/analogControlWithPotentiometer.ino
+++ Tinkercad/Analog/AnalogControlWithPotentiometer/analogControlWithPotentiometer.ino
@@ -1,11 +1,12 @@
-
+#define led_pin A1
+#define meter_pin A0
 int sensorValue = 0;
 int outputValue = 0;
 
 void setup()
 {
-  pinMode(A0, INPUT);
-  pinMode(9, OUTPUT);
+  pinMode(meter_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -13,7 +14,7 @@ void loop()
 {
   sensorValue = analogRead(A0);
   outputValue = map(sensorValue, 0, 1023, 0, 255);
-  analogWrite(9, outputValue);
+  analogWrite(led_pin, outputValue);
   Serial.print("sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t output = ");
